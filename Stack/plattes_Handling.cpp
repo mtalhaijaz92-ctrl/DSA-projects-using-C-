f00267773567a1317d2adafdd0786bed9725eb1c
M Talha Ijaz <mtalhaijaz92@gmail.com>
2026-09-15 19:54:29 +0500

    Add Restaurant class for plate management using stack
    Implemented a Restaurant class to manage plates using a stack. Added methods to add, remove, and display plates, as well as to show the total number of plates.

--- Stack/plattes_Handling.cpp
+++ Stack/plattes_Handling.cpp
@@ -1,1 +1,79 @@
 
+#include <iostream>
+#include <stack>
+#include <string>
+using namespace std;
+
+class Restaurant
+{
+private:
+    stack<string> plates;
+
+public:
+
+    // Plate add karna
+    void addPlate(string plate)
+    {
+        plates.push(plate);
+        cout << plate << " All Plattes are Placed in the Stack Succesfully.\n";
+    }
+
+    // Top plate uthana
+    void removePlate()
+    {
+        if (plates.empty())
+        {
+            cout << "There is a no Plattes.\n";
+            return;
+        }
+
+        cout << plates.top() << " plate is Picked .\n";
+        plates.pop();
+    }
+
+    // Top plate dekhna
+    void showTopPlate()
+    {
+        if (plates.empty())
+        {
+            cout << "Stack is empty.\n";
+            return;
+        }
+
+        cout << "Top plate: "
+             << plates.top() << endl;
+    }
+
+    // Total plates
+    void totalPlates()
+    {
+        cout << "Total plates: "
+             << plates.size() << endl;
+    }
+};
+
+int main()
+{
+    Restaurant restaurant;
+
+    restaurant.addPlate("Plate 1");
+    restaurant.addPlate("Plate 2");
+    restaurant.addPlate("Plate 3");
+
+    cout << "\n";
+
+    restaurant.showTopPlate();
+
+    cout << "\n";
+
+    restaurant.removePlate();
+    restaurant.removePlate();
+
+    cout << "\n";
+
+    restaurant.showTopPlate();
+
+    restaurant.totalPlates();
+
+    return 0;
+}
